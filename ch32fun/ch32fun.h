5e586f814410d42be3fc65a50d8bbbf611b9849c
dwillmore <10374140+dwillmore@users.noreply.github.com>
2025-03-14 18:42:04 -0400

    Changes to allow 00x chips and 003 chips to build with combined .mk logic

--- ch32fun/ch32fun.h
+++ ch32fun/ch32fun.h
@@ -172,7 +172,7 @@
 #ifndef HSI_VALUE
 	#if defined(CH32V003)
 		#define HSI_VALUE                 (24000000) // Value of the Internal oscillator in Hz, default.
-	#elif defined(CH32V006)
+	#elif defined(CH32V00x)
 		#define HSI_VALUE				  (24000000)
 	#elif defined(CH32X03x)
 		#define HSI_VALUE				  (48000000)
