2c380470c34742f237ec21aa420eb45078af9e28
Outfisken <matter.of.time2011@gmail.com>
2025-04-24 17:51:36 +0300

    add rtc example

---
+++ examples_v30x/rtc/README.md
@@ -0,0 +1,2 @@
+# RTC
+This example demonstrates how to configure the RTC and set an alarm interrupt.
---
+++ examples_v30x/rtc/funconfig.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCONFIG_H
+#define _FUNCONFIG_H
+
+#define CH32V303           		1
+
+#endif
