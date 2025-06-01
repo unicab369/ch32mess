c97a5b714f338ce2537907614366acd03d422361
Harchimemedes <474890266@qq.com>
2025-06-01 10:26:02 +0800

    fix uartdemo clock setting

--- examples_ch5xx/uartdemo/funconfig.h
+++ examples_ch5xx/uartdemo/funconfig.h
@@ -3,7 +3,7 @@
 
 #define FUNCONF_USE_HSI           0 // CH5xx does not have HSI
 #define FUNCONF_USE_HSE           1
-#define CLK_SOURCE_CH55X          CLK_SOURCE_PLL_60MHz // default so not really needed
+#define CLK_SOURCE_CH5XX          CLK_SOURCE_PLL_60MHz // default so not really needed
 #define FUNCONF_SYSTEM_CORE_CLOCK 60 * 1000 * 1000     // keep in line with CLK_SOURCE_CH55X
 
 #define FUNCONF_DEBUG_HARDFAULT   0
