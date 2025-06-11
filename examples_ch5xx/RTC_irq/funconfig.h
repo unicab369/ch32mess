fc7571488e26366d10720d932bb7abba555fa3ff
Lars Beemster <biemster@users.noreply.github.com>
2025-06-11 20:03:10 +0200

    support for ch573/1 in RTC_irq demo, set ch573 vector to the jumpy type

--- examples_ch5xx/RTC_irq/funconfig.h
+++ examples_ch5xx/RTC_irq/funconfig.h
@@ -9,7 +9,7 @@
 #define FUNCONF_DEBUG_HARDFAULT   0
 #define FUNCONF_USE_CLK_SEC       0
 #define FUNCONF_USE_DEBUGPRINTF   0 // saves 16 bytes, enable / remove if you want printf over swio
-#define FUNCONF_USE_UARTPRINTF    1
+#define FUNCONF_USE_UARTPRINTF    0
 #define FUNCONF_INIT_ANALOG       0 // ADC is not implemented yet
 
 #endif
